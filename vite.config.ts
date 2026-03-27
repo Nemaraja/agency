2b53f63b48f7f15f8e1fc858d5619635fdfc5b66
BDRS <arajanemarloro@gmail.com>
2026-03-27 20:12:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/agency/', // This MUST match your repo name exactly
   plugins: [react()],
-  base: '/agency/', 
 })
